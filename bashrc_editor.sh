f656a11c9d08312dbd044c06d819922d01518a23
ZamyrDev-123 <zamirlcastror@gmail.com>
2024-12-30 19:25:14 -0400

    MyBash command for Termux
    by ZamyrDev for Termux (Android)
    Version 0.0.4

---
+++ bashrc_editor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Created by ZamyrDev for Termux (Android)
+
+# Paste the following texto in the $PREFIX/etc/bash.bashrc
+# or type:
+#    echo '[Paste texto here]' >> $PATH/etc/bash.bashrc
+
+echo -e "\n"
+echo 'bash.bashrc on $PREFIX/etc/bash.bashrc'
+echo -e 'call with "mybash" command \n'
+alias mybash='nano $PREFIX/etc/bash.bashrc'
+
+# extra
+alias mkcd='mkdir $1 && cd $1'
